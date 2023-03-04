4c94f3b61ad94d0d051cfd37ed8db2f9de23927e
Bello Soliu <bellosoliu12@gmail.com>
2023-03-04 22:26:50 +0100

    updates

--- ConsoleAtmData/AtmDbContextFactory.cs
+++ ConsoleAtmData/AtmDbContextFactory.cs
@@ -7,6 +7,7 @@ namespace EFCoreATM_Data;
 public class AtmDbContextFactory : IDesignTimeDbContextFactory<AtmDbContext>
 {
     //string _connectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+
     string _connectionString = @"Data Source=DESKTOP-APMJTIG;Initial Catalog = efCoreAtmDB; Integrated Security =True; Encrypt=false";
     public AtmDbContextFactory() { }
 
